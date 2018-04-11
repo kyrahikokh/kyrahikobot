727393c40d20f341e63a1b841a2273b032866709
kyrahikokh <38015924+kyrahikokh@users.noreply.github.com>
2018-04-11 09:52:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,6 +47,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'who is renelephant?') {
+    	message.reply('His true name is Ravren and is Jims target in PUBG.');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
